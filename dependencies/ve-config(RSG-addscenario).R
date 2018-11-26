b8d0e5cd7d4608ad892133aa22cdd4eaad095e31
Jeremy Raw <jeremy@jeremyraw.com>
2018-11-26 12:39:04 -0500

    Ignoring local VE-config.R and VE-dependencies.csv. Ignoring build output files with any likely name. Added website publication to Makefile.

--- dependencies/ve-config(RSG-addscenario).R
+++ dependencies/ve-config(RSG-addscenario).R
@@ -2,7 +2,7 @@
 # The key item is the root 
 
 # Required: root directory of the VisionEval version described in VE-dependencies.csv
-ve.root <- "c:\\users\\jeremy.DESKTOP-OLMEAON\\Documents\\Git-Repos\\visioneval"
+ve.root <- "c:\\users\\jeremy.DESKTOP-OLMEAON\\Documents\\Git-Repos\\VisionEval-RSG"
 
 # Optional: location in which "built-packages", "miniCRAN" and "runtime" will be built
 # Defaults to putting those packages in an "installer_YYMMDD_HHMM" folder in ve.install
